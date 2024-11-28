5efce7b802833b2d73234e3829184e18fa2b8336
syesa <syesaya12@gmail.com>
2024-11-28 17:11:22 +0700

    testing

--- app/src/main/java/com/example/projectuaspiranti/MainActivity.kt
+++ app/src/main/java/com/example/projectuaspiranti/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        //jkkljlkj
         auth = Firebase.auth
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
